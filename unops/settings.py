5961a713a5671a721ea55fa850e0aef6856e3097
muriithiderro <muriithiderrick56@gmail.com>
2018-09-10 02:58:08 +0300

    ss

--- unops/settings.py
+++ unops/settings.py
@@ -17,7 +17,7 @@ from decouple import config
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-ALLOWED_HOSTS = ['localhost', 'herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['localhost', 'unops.herokuapp.com', '127.0.0.1']
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', cast=bool)
 
